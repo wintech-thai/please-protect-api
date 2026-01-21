837364096c52dfd87bb595cbc4086ca6406e4966
pjames <pjames@gmail.com>
2026-01-21 14:13:54 +0700

    Added MIoC

--- please-protect-api/Migrations/20260121065751_IoC_001.Designer.cs
+++ please-protect-api/Migrations/20260121065751_IoC_001.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Its.PleaseProtect.Api.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace otep.api.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20260121065751_IoC_001")]
+    partial class IoC_001
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
